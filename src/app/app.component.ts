cece01b8e50bc6af7a8c071ddff82be6460c7ab8
Mihlali Kota <133135575+MihlaliKota@users.noreply.github.com>
2024-10-20 01:33:27 +0200

    22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { BorrowComponent } from './borrow/borrow.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent, BorrowComponent],
+  imports: [RouterOutlet, LandingComponent, BorrowComponent,],
   templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css'],
   //  './landing.css'
@@ -15,3 +22,16 @@ import { BorrowComponent } from './borrow/borrow.component';
 export class AppComponent {
   title = 'Community Savings Platform';
 }
+
+
+
+
+@NgModule({
+  declarations: [AppComponent, VotingPollComponent],
+  imports: [
+   
+    // other necessary imports
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
